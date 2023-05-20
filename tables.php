3f6ba2b2e722b2b50b8116973dac0642cb2c96f7
tarikBakir <94920690+tarikBakir@users.noreply.github.com>
2023-05-20 15:51:07 +0300

    added

--- tables.php
+++ tables.php
@@ -1,70 +1,22 @@
+extension=php_mysqli.so
+
 <?php
-  
 
-// Create connection
-//$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
 }
-echo "connection success";
-// sql to create table
+$servername ="localhost";
+$username = "root";
+$password ="admin";
+$conn = new mysqli($servername, $username,$password);
 
-$table1 = "CREATE TABLE WORKER(
-    id INT UNSIGNED AUTO_INCREAMENT PRIMARY KEY ,
-    firstname VARCHAR(60),
-    lastname VARCHAR(60),
-    phonenumber VARCHAR(255),
-    description VARCHAR(255),
-    email VARCHAR(255),
+if($conn->connect_error){
+    die("Connection failed!". $conn->connect_error);
 
-)";
-
-$table2 = "CREATE TABLE profession(
-    id INT UNSIGNED AUTO_INCREAMENT PRIMARY KEY ,
-    jobname VARCHAR(60),
-    FOREIGN KEY (category_id)REFERENCES category(id)
-  
-
-)";
-
-$table3 = "CREATE TABLE category(
-    id INT UNSIGNED AUTO_INCREAMENT PRIMARY KEY ,
-    categoryname VARCHAR(60),
-  
-
-)";
-
-$table4 = "CREATE TABLE review(
-    id INT UNSIGNED AUTO_INCREAMENT PRIMARY KEY ,
-    credibility VARCHAR(60),
-    Punctual  VARCHAR(60),
-    proficiency VARCHAR(60),
-    cost VARCHAR(60),
-    FOREIGN KEY (worker_id)REFERENCES WORKER(id),
-
-)";
-
-$table5 = "CREATE TABLE has_job(
-    FOREIGN KEY (worker_id) REFERENCES WORKER(id),
-    FOREIGN KEY (profession_id)REFERENCES profession(id)
-)";
-
-$tables = [$table1, $table2, $table3,$table4,$table5];
-
-foreach($tables as $k => $sql){
-    $query = @$conn->query($sql);
-
-    if(!$query)
-       $errors[] = "Table $k : Creation failed ($conn->error)";
-    else
-       $errors[] = "Table $k : Creation done";
+}else {
+    echo "successful";
 }
-
-foreach($errors as $msg) {
-    echo "$msg <br>";
- }
-
-    $conn->close();
-    ?>
-
+$conn->close();
+?>
