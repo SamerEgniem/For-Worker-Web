dab0a647a5c57908375355a1aa4b0d4b0d743711
tarikBakir <94920690+tarikBakir@users.noreply.github.com>
2023-05-20 18:02:06 +0300

    created the database officially
    * created the database
    * created tables
    *inserted a data into tables to see if its working well

--- tables.php
+++ tables.php
@@ -1,6 +1,9 @@
-extension=php_mysqli.so
+
 
 <?php
+
+
+
 
 if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
     echo 'We don\'t have mysqli!!!';
@@ -10,13 +13,49 @@ if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
 $servername ="localhost";
 $username = "root";
 $password ="admin";
-$conn = new mysqli($servername, $username,$password);
+$dbname  = "fwdb";
 
-if($conn->connect_error){
-    die("Connection failed!". $conn->connect_error);
+$conn = new mysqli($servername, $username,$password,$dbname,3306);
 
-}else {
-    echo "successful";
+$sql = "CREATE TABLE Category (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY, 
+        category_name VARCHAR(60) NOT NULL
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
 }
+sleep(2);
+$sql1 = "CREATE TABLE Profession (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY,
+        job_name NVARCHAR(255),
+        category_id INT,
+    FOREIGN KEY (category_id) REFERENCES Category(id)
+)";
+
+sleep(2);
+if ($conn->query($sql1) === TRUE) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$sqldata = "INSERT INTO Category (category_name)
+VALUES ('cars')";
+if ($conn->query($sqldata) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sqldata . "<br>" . $conn->error;
+}
+$sqldata = "INSERT INTO Profession (job_name,category_id)
+VALUES ('parts',1)";
+
+if ($conn->query($sqldata) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sqldata . "<br>" . $conn->error;
+}
+
 $conn->close();
 ?>
